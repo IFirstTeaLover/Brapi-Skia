452f98c18d1797751f88b066db9d152e88d326b7
BulbaSprout <pasabozenko3@gmail.com>
2026-05-19 19:38:32 +0300

    COOL stuff

--- src/client/java/dev/bsprout/brapi/client/Brapi.java
+++ src/client/java/dev/bsprout/brapi/client/Brapi.java
@@ -5,28 +5,34 @@ import com.mojang.blaze3d.platform.DepthTestFunction;
 import com.mojang.blaze3d.shaders.UniformType;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandManager;
 import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderPipelines;
 import net.minecraft.resources.Identifier;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.server.packs.PackType;
+import net.minecraft.server.packs.resources.ResourceManager;
 
 public class Brapi implements ClientModInitializer {
 
-    // Here we register a custom render pipeline for rounded rects (and circles !!)
-    public static final RenderPipeline ROUNDED_RECT_PIPELINE = RenderPipelines.register(
-            RenderPipeline.builder(RenderPipelines.GUI_SNIPPET)
-                    .withLocation(Identifier.fromNamespaceAndPath("brapi", "pipeline/rounded_rect"))
-                    .withVertexShader("core/brapi_rounded_rect")
-                    .withFragmentShader("core/brapi_rounded_rect")
-                    .withUniform("RectData", UniformType.UNIFORM_BUFFER)
-                    .withBlend(BlendFunction.TRANSLUCENT)
-                    .withDepthTestFunction(DepthTestFunction.NO_DEPTH_TEST)
-                    .build()
-    );
+    public static RenderPipeline ROUNDED_RECT_PIPELINE = null;
+    public static boolean pipelinesReady = false;
 
 	@Override
 	public void onInitializeClient() {
+        ResourceManagerHelper.get(PackType.CLIENT_RESOURCES).registerReloadListener(new SimpleSynchronousResourceReloadListener() {
+            @Override
+            public Identifier getFabricId() {
+                return Identifier.fromNamespaceAndPath("brapi", "pipelines");
+            }
+
+            @Override
+            public void onResourceManagerReload(ResourceManager manager) {
+                initPipelines();
+            }
+        });
         // Adding a simple test command to open our Test screen (dev/bsprout/brapi/client/Test.java)
         ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> dispatcher.register(ClientCommandManager.literal("brapitest")
                 .executes(context -> {
@@ -40,4 +46,22 @@ public class Brapi implements ClientModInitializer {
                     return 1;
                 })));
 	}
+
+    public static void initPipelines() {
+        ROUNDED_RECT_PIPELINE = RenderPipelines.register(
+                RenderPipeline.builder(RenderPipelines.GUI_SNIPPET)
+                        .withLocation(Identifier.fromNamespaceAndPath("brapi", "pipeline/rounded_rect"))
+                        .withVertexShader("core/brapi_rounded_rect")
+                        .withFragmentShader("core/brapi_rounded_rect")
+                        .withUniform("RectData", UniformType.UNIFORM_BUFFER)
+                        .withBlend(BlendFunction.TRANSLUCENT)
+                        .withDepthTestFunction(DepthTestFunction.NO_DEPTH_TEST)
+                        .build()
+        );
+        pipelinesReady = true;
+    }
+
+    public static boolean isReady() {
+        return pipelinesReady;
+    }
 }
